76bb255761e234c4975363ad94ec0e0103f966a7
Rick <ricksmycodeguy@gmail.com>
2021-10-27 09:17:39 -0500

    Task 2 wip

---
+++ U5-M3-Homework-PY-III/Task2.py
@@ -0,0 +1,18 @@
+"""
+Using Big O notation, what is the correct 
+classification of space complexity for the fuction below
+
+My Answer is 
+"""
+    
+def do_a_couple_things(n): ## my guess hence is O(2n)
+    my_list = [] ## my guess O(1)
+    my_second_list = [0] * 26 ## my guess O(1)
+    
+    for _ in range(n):
+        my_list.append("lambda") 
+        print(my_second_list[n % 25])
+        
+    return my_list
+
+print(do_a_couple_things())
